b24efb1d3327bc407a16a7204b930f4daf4e2813
Meredith Nachman <mwnachman@gmail.com>
2016-11-15 17:01:54 -0800

    Add login post request to receive user data

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
+import $ from 'jquery';
 
 
 
@@ -11,6 +12,8 @@ class Login extends React.Component {
     this.state = {
       email: '',
       password: '',
+      username: '999888777666',
+      urlPrefix: 'http://localhost:1337'
     };
   }
 
@@ -22,47 +25,56 @@ class Login extends React.Component {
   }
 
   submitLoginForm(e) {
-    console.log(e.target);
+    const authInfo = {
+      'account': {
+        'amazonId': this.state.username
+      }
+    };
+    console.log('authInfo', JSON.stringify(authInfo));
+    $.ajax({
+      url: this.state.urlPrefix + '/login',
+      type: 'POST',
+      dataType: 'application/json',
+      data: authInfo,
+      complete: function (data) {
+        console.log('Logged in:' + JSON.stringify(data));
+      }
+    });
   }
 
   render() {
     return (
       <div>
         <h2>Log In</h2>
-        <form>
-          <table>
-            <tbody>
-              <tr>
-                <td>Email</td>
-                <td><input type='text' name='email'
-                  onChange={this.handleInputChange.bind(this)}/>
-                </td>
-              </tr>
-              <tr>
-                <td>Password</td>
-                <td><input type='password' name='password'
-                  onChange={this.handleInputChange.bind(this)}/>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <button onClick={this.submitLoginForm.bind(this)}>Login</button>
-        </form>
+        <button onClick={this.submitLoginForm.bind(this)}>Login</button>
         <span>Need an account? </span>
         <Link to='/signup'>Sign up</Link>
       </div>
     );
   } 
-}
 
+}
 
 export default Login;
 
 
+  //       <form>
+  //         <table>
+  //           <tbody>
+  //             <tr>
+  //               <td>Email</td>
+  //               <td><input type='text' name='email'
+  //                 onChange={this.handleInputChange.bind(this)}/>
+  //               </td>
+  //             </tr>
+  //             <tr>
+  //               <td>Password</td>
+  //               <td><input type='password' name='password'
+  //                 onChange={this.handleInputChange.bind(this)}/>
+  //               </td>
+  //             </tr>
+  //           </tbody>
+  //         </table>
 
-
-
-
-
-
-  
+  //       </form>
+  // 
