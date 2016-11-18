212c47dd2805742666553bf64fd7bde4843cf774
Meredith Nachman <mwnachman@gmail.com>
2016-11-18 12:42:47 -0800

    Add CSS to nav bar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,26 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { Grid, Navbar, Jumbotron, Button, Row } from 'react-bootstrap';
-
-// import './User.css';
-
-export const Nav = () => (
-  <div className='container'>
-    <Navbar inverse fixedTop>
-      <Grid>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to='/app/home'>myNanny</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-      </Grid>
-    </Navbar>
-  </div>
-); 
+import { Grid, Navbar, Nav, NavItem, NavDropdown, MenuItem, Jumbotron, Button } from 'react-bootstrap';
 
 
+class Navigation extends React.Component {
 
-export default Nav;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      
+    };
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <Navbar inverse fixedTop>
+          <Grid>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <Link to='/app/home'>myNanny</Link>
+              </Navbar.Brand>
+              <Nav>
+                <NavItem><Link to='/'>Landing</Link></NavItem>
+                <NavItem><Link to='/app/home'>Home</Link></NavItem>
+                <NavItem><Link to='/app/account'>Account</Link></NavItem>
+                <NavItem><Link to='/app/kids'>Kids</Link></NavItem>
+                <NavItem><Link to='/app/logout'>Logout</Link></NavItem>
+                <NavItem pullRight>
+                  <NavDropdown title='Menu' id='basic-nav-dropdown'>
+                    <MenuItem><Link to='app/account'>Account</Link></MenuItem>
+                    <MenuItem divider />
+                    <MenuItem><Link to='app/logout'>Log Out</Link></MenuItem>
+                  </NavDropdown>
+                </NavItem>
+              </Nav>
+              <Navbar.Toggle />
+            </Navbar.Header>
+          </Grid>
+        </Navbar>
+      </div>
+    );
+  }
+
+}
+
+export default Navigation;
 
       
