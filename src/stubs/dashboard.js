6509badac60757bd74dbcbd94d2f22ee7f829ba7
Eugene Choe <eugene.choe@gmail.com>
2016-11-23 10:49:07 -0800

    Change stubs format for more modularity with Luis' approval Use stubs for all data in frontend!!!

--- src/stubs/dashboard.js
+++ src/stubs/dashboard.js
@@ -8,28 +8,14 @@ module.exports = {
       'name': 'Winston',
       'photo': 'http://photo.jpg',
       'checkedIn': null,
-      'curfew': '15:00',
-      // Today's chores only
-      'chores': [
-        {
-          'id': 3,
-          'title': 'Clean your room',
-          'completed': false
-        },
-        {
-          'id': 4,
-          'title': 'Wash the dishes',
-          'completed': true
-        }
-      ],
+      'curfew': '15:00'
     },
     {
       'id': 2,
       'name': 'Wendy',
       'photo': 'http://photo1.jpg',
-      'checkedIn': '16:43',
-      'curfew': '17:00',
-      'chores': []
+      'checkedIn': '2016-11-30',
+      'curfew': '17:00'
     }
   ]
 };
---
+++ src/stubs/schedule.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'child': {
+    'id': 1,
+    'name': 'Winston',
+    'schedule': {
+      'id': 15,
+      'sunday': 'null',
+      'monday': 'null',
+      'tuesday': '12:30',
+      'wednesday': '14:05',
+      'thursday': '18:30',
+      'friday': '14:00',
+      'saturday': '09:30'
+    }
+  }
+};
