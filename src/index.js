a1ba824761a93cf6fbac814eef7183ac43b1059b
Meredith Nachman <mwnachman@gmail.com>
2016-11-28 14:23:39 -0800

    Add middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { browserHistory } from 'react-router';
 import allReducers from './reducers';
 import Routes from './routes';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
-var store = createStore(allReducers);
+const store = createStore(allReducers, applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+);
+
+const loggerMiddleware = createLogger();
 
 ReactDOM.render(
   <Provider store={store}>
