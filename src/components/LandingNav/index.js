6ee6f65b6a95c5df7f5efda17d94ca2afecb0731
Luis Villeda <luisevilleda@gmail.com>
2016-12-01 12:11:57 -0800

    Style homepage, edit navbar

---
+++ src/components/LandingNav/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Button } from 'react-bootstrap';
+import './styles.css';
+
+const amazonButtonImage = 'https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png';
+
+const amazonLogin = () => {
+  const options = { scope: 'profile' };
+  amazon.Login.authorize(options, '/app/home');
+  return false;
+};
+
+const navbarInstance = (
+  <Navbar fixedTop >
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href='#'>myNanny</a>
+      </Navbar.Brand>
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav pullRight className='navRight'>
+        <img
+        id='amazonButton'
+        alt='Login with Amazon'
+        src={amazonButtonImage}
+        onClick={amazonLogin.bind(this)}
+        />
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+export default navbarInstance;
