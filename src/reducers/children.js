934fbc3a42b2390cabe6cfff12e23bf2f0dddb28
Eugene Choe <eugene.choe@gmail.com>
2016-11-30 19:44:50 -0800

    Complete dashboard store data load and presentation

--- src/reducers/children.js
+++ src/reducers/children.js
@@ -1,27 +1,34 @@
 const ChildrenReducer = (state = {}, 
   action) => {
   if ( action.type === 'TOGGLE_CHILD_EDIT' ) {
-    // console.log('TOGGLE CHILD EDIT Reducer');
     var obj = {};
     var key = action.payload.id;
-    obj[key] = { name: action.payload.name,
-                 phone: action.payload.phone,
-                 show: action.payload.show,
-                 editable: !action.payload.editable };
+    obj[key] = {
+      name: action.payload.name,
+      phone: action.payload.phone,
+      show: action.payload.show,
+      editable: !action.payload.editable
+    };
     return Object.assign({}, state, obj);
+  } else if ( action.type === 'REQUEST_CHILDREN' ) {
+    return Object.assign({}, state, {
+      isFetching: true
+    });
   } else if ( action.type === 'RECEIVE_CHILDREN' ) {
-    let childObj = {};
+    console.log('i got some kids', action.payload);
+    let list = {};
     let kids = action.payload.children;
     for (var i = 0; i < kids.length; i++) {
       let child = action.payload.children[i];
-      childObj[child.id] = {
+      list[child.id] = {
         name: child.name,  
         phone: child.phone,
+        photo: child.photo,
         show: true,
         editable: false
       };
     }
-    return childObj;
+    return list;
   } else if ( action.type === 'TOGGLE_CHILD_SHOW' ) {
     console.log('TOGGLE CHILD SHOW Reducer');
     var obj = {};
